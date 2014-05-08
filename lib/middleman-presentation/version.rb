1104b36e3d040c50d8fac2198108327b46f86455
Max Meyer <dev@fedux.org>
2014-05-08 15:27:39 +0200

    version bump to 0.0.10

--- lib/middleman-presentation/version.rb
+++ lib/middleman-presentation/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Middleman
   module Presentation
-    VERSION = '0.0.9'
+    VERSION = '0.0.10'
   end
 end
--- lib/presentation/version.rb
+++ lib/presentation/version.rb
@@ -1,4 +1,4 @@
 #main Presentation
 module Presentation
-  VERSION = '0.0.9'
+  VERSION = '0.0.10'
 end
