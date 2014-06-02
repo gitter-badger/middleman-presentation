c8244277187be473ff07ec804ea5c897433eef8e
Max Meyer <dev@fedux.org>
2014-06-02 21:30:47 +0200

    First steps to support image galleries

--- lib/middleman-presentation/helpers.rb
+++ lib/middleman-presentation/helpers.rb
@@ -2,7 +2,6 @@
 module Middleman
   module Presentation
     module Helpers
-
       # Yield slides
       def yield_slides
         Presentation::Slide.clear
@@ -11,6 +10,22 @@ module Middleman
         Presentation::Slide.all.each do |s|
           partial s.relative_to_path(root)
         end
+      end
+
+      def image_gallery(images, title = nil, image_gallery_id = nil)
+        @image_gallery_id = image_gallery_id ||= SecureRandom.hex
+
+        template = <<-EOS.strip_heredoc
+         <a href="<% image_path %>" data-lightbox="<% image_gallery_id %>">
+           <img src="<% image_path %>" alt="<% title %>" class="fd-preview-image">
+         </a>
+        EOS
+
+        images.each do |i|
+          engine = Erubis::Eruby.new(template)
+          result << engine.result(image: i, image_gallery_id: image_gallery_id, title: title)
+        end
+
       end
     end
   end
