bb0165395397c692479731d8f6a03c02d6b233fc
Dennis Günnewig <dg1@vrnetze.de>
2014-07-21 09:36:19 +0200

    Some cleanup

--- lib/middleman-presentation/version.rb
+++ lib/middleman-presentation/version.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 module Middleman
   module Presentation
-    VERSION = '0.0.10'
+    VERSION = '0.1.1'
   end
 end
--- lib/presentation/version.rb
+++
@@ -1,5 +0,0 @@
-# encoding: utf-8
-# Presentation
-module Presentation
-  VERSION = '0.1.1'
-end
