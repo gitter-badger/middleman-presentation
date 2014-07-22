ffc93212562c411655d74e079d5953ceac9f03c9
Dennis Günnewig <dg1@vrnetze.de>
2014-07-22 15:09:20 +0200

    Fixed error message

--- lib/middleman-presentation/helpers/images.rb
+++ lib/middleman-presentation/helpers/images.rb
@@ -29,9 +29,9 @@ module Middleman
         result = sprockets.each_file.find { |f| f.to_s.include? substring }
 
         if result.blank? 
-          I18n.t('errors.asset_not_found', substring)
+          I18n.t('errors.asset_not_found', asset: substring)
         else
-          result.relative_path_from(Pathname.new(source_dir))
+          result.relative_path_from(Pathname.new(source_dir)).to_s
         end
       end
     end
