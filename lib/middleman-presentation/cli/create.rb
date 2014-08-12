2fb5ea11474edb327d84ecc55bae91c13bb5f64b
Dennis Günnewig <dg1@vrnetze.de>
2014-08-12 22:01:50 +0200

    Fixed offence

--- lib/middleman-presentation/cli/create.rb
+++ lib/middleman-presentation/cli/create.rb
@@ -4,7 +4,6 @@ module Middleman
     module Cli
       # This class provides an 'presentation init' command for the middleman CLI.
       class Create < Thor
-
         register(CreateTheme, 'theme', 'theme NAME', 'Create a new theme named NAMED')
         register(CreatePresentation, 'presentation', 'presentation [DIR]', 'Initialize a new presentation in DIR (default: $PWD)')
 
