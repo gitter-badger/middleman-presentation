7961f34bd6d284837797d30543fd8544a9f91df6
Max Meyer <dev@fedux.org>
2014-08-28 07:17:10 +0200

    init

---
+++ lib/middleman/presentation/helpers.rb
@@ -0,0 +1,9 @@
+require "middleman/presentation/helpers/version"
+
+module Middleman
+  module Presentation
+    module Helpers
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/middleman/presentation/helpers/version.rb
@@ -0,0 +1,7 @@
+module Middleman
+  module Presentation
+    module Helpers
+      VERSION = "0.0.1"
+    end
+  end
+end
