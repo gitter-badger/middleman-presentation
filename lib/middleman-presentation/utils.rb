9e71f48c5ebd8b097a29d272044e47bc325c7b37
Max Meyer <dev@fedux.org>
2014-09-02 07:19:19 +0200

    Change config of zip

--- lib/middleman-presentation/utils.rb
+++ lib/middleman-presentation/utils.rb
@@ -14,6 +14,13 @@ module Middleman
       #   A prefix for the zip file, e.g. dir1/dir2/ => dir1/dir2/zip_file.
       #   Please mind the trailing '/'.
       def zip(source_directory, destination_file, prefix: nil)
+        Zip.setup do |c|
+          c.on_exists_proc          = true
+          c.continue_on_exists_proc = true
+          c.unicode_names           = true
+          c.default_compression     = Zlib::BEST_COMPRESSION
+        end
+
         Zip::File.open(destination_file, Zip::File::CREATE) do |file|
           Dir.glob(File.join(source_directory, '**', '*')).each do |filename|
 
