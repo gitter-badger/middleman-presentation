7fac16d4e0f569bcb041da3b582964e1c34a60f5
Max Meyer <dev@fedux.org>
2014-09-02 20:33:07 +0200

    Use 60%

--- lib/middleman-presentation/presentation_config.rb
+++ lib/middleman-presentation/presentation_config.rb
@@ -24,7 +24,7 @@ module Middleman
       option :activate_slide_number, true
       option :default_transition_type, 'linear'
       option :default_transition_speed, 'default'
-      option :presentation_size, %w(75% 75%)
+      option :presentation_size, %w(60% 60%)
       option :install_assets, true
       option :initialize_git, true
       option :check_for_bower, true
