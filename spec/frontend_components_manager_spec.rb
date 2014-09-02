8af4445f2fdb17689ba9c55005089ec1dc36e310
Max Meyer <dev@fedux.org>
2014-09-02 20:31:16 +0200

    Add test about order of components

--- spec/frontend_components_manager_spec.rb
+++ spec/frontend_components_manager_spec.rb
@@ -109,6 +109,33 @@ RSpec.describe FrontendComponentsManager do
 
       expect(manager.available_frontend_components.first.name).to eq 'test1'
     end
+
+    it 'returns the frontend components in the order they were added', :focus do
+      components = []
+
+      5.times do |i|
+        components << FrontendComponent.new(
+          name: "test#{i - 1}",
+          resource_locator: 'http://www.example.com',
+        )
+      end
+
+      manager = FrontendComponentsManager.new
+      manager.add(components[0])
+      manager.add(components[2])
+      manager.add(components[4])
+      manager.add(components[1])
+      manager.add(components[3])
+      manager.add(components[3])
+      manager.add(components[4])
+      manager.add(components[4])
+
+      expect(manager.available_frontend_components[0]).to be components[0]
+      expect(manager.available_frontend_components[1]).to be components[2]
+      expect(manager.available_frontend_components[2]).to be components[4]
+      expect(manager.available_frontend_components[3]).to be components[1]
+      expect(manager.available_frontend_components[4]).to be components[3]
+    end
   end
 
   context '#to_s' do
