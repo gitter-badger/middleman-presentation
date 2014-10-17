70ff20272612b5ac0255d8e5aad3a7b6d1c47b95
Dennis Günnewig <dg1@vrnetze.de>
2014-10-17 07:57:14 +0200

    Fixed spec

--- middleman-presentation-core/spec/assets_manager_spec.rb
+++ middleman-presentation-core/spec/assets_manager_spec.rb
@@ -3,24 +3,27 @@ require 'spec_helper'
 
 RSpec.describe AssetsManager do
   let(:manager) { AssetsManager.new }
+  let(:asset) { instance_double('Middleman::Presentation::Asset') }
+  let(:list) { instance_double('Middleman::Presentation::AssetList') }
 
   context '#load_from_list' do
     it 'loads assets from list' do
-      asset = instance_double('Middleman::Presentation::Asset')
+      allow(list).to receive(:each) { |&block| [asset].each(&block) }
 
-      manager.load_from_list [asset]
+      manager.load_from_list list 
       expect(manager).to be_know asset
     end
   end
 
   context '#each_loadable_asset' do
     it 'iterates over all assets' do
-      asset = instance_double('Middleman::Presentation::Asset')
       allow(asset).to receive(:valid?).and_return(true)
       allow(asset).to receive(:loadable?).and_return(true)
       allow(asset).to receive(:source_path).and_return('image1.png')
 
-      manager.load_from_list [asset]
+      allow(list).to receive(:each) { |&block| [asset].each(&block) }
+
+      manager.load_from_list list
 
       output = capture :stdout do
         manager.each_loadable_asset { |a| puts a.source_path }
@@ -44,7 +47,9 @@ RSpec.describe AssetsManager do
       allow(asset2).to receive(:stylesheet?).and_return(true)
       allow(asset2).to receive(:source_path).and_return('stylesheet.css')
 
-      manager.load_from_list [asset1, asset2]
+      allow(list).to receive(:each) { |&block| [asset1, asset2].each(&block) }
+
+      manager.load_from_list list
 
       output = capture :stdout do
         manager.each_importable_stylesheet { |a| puts a.source_path }
@@ -69,7 +74,9 @@ RSpec.describe AssetsManager do
       allow(asset2).to receive(:script?).and_return(true)
       allow(asset2).to receive(:source_path).and_return('script.js')
 
-      manager.load_from_list [asset1, asset2]
+      allow(list).to receive(:each) { |&block| [asset1, asset2].each(&block) }
+
+      manager.load_from_list list
 
       output = capture :stdout do
         manager.each_importable_javascript { |a| puts a.source_path }
@@ -82,13 +89,14 @@ RSpec.describe AssetsManager do
 
   context '#to_s' do
     it 'returns a string representation of self' do
-      asset1 = instance_double('Middleman::Presentation::Asset')
-      allow(asset1).to receive(:source_path).and_return('image1.png')
-      allow(asset1).to receive(:destination_directory).and_return('output.d')
-      allow(asset1).to receive(:loadable?).and_return(true)
-      allow(asset1).to receive(:importable?).and_return(true)
+      allow(asset).to receive(:source_path).and_return('image1.png')
+      allow(asset).to receive(:destination_directory).and_return('output.d')
+      allow(asset).to receive(:loadable?).and_return(true)
+      allow(asset).to receive(:importable?).and_return(true)
 
-      manager.load_from_list [asset1]
+      allow(list).to receive(:each) { |&block| [asset].each(&block) }
+
+      manager.load_from_list list
 
       expect(manager.to_s).to eq <<-EOS.strip_heredoc.chomp
         +-------------+-----------------------+----------+------------+
