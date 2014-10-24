c87f14590e498d1ce7d52c127cfdc5b2f435e0c1
Max Meyer <dev@fedux.org>
2014-10-24 21:05:06 +0200

    Add new plugin to helpers

--- middleman-presentation-helpers/lib/middleman-presentation-helpers.rb
+++ middleman-presentation-helpers/lib/middleman-presentation-helpers.rb
@@ -4,6 +4,7 @@ require 'middleman-presentation-helpers/helpers/image_gallery'
 require 'middleman-presentation-helpers/helpers/image'
 require 'middleman-presentation-helpers/helpers/slides'
 
+require 'middleman-presentation-helpers/plugins/normalize_plugin'
 require 'middleman-presentation-helpers/plugins/jquery_plugin'
 require 'middleman-presentation-helpers/plugins/slides_plugin'
 require 'middleman-presentation-helpers/plugins/images_plugin'
---
+++ middleman-presentation-helpers/lib/middleman-presentation-helpers/plugins/normalize_plugin.rb
@@ -0,0 +1,18 @@
+# encoding: utf-8
+module Middleman
+  module Presentation
+    # Helpers plugin
+    module Helpers
+      # Slides plugin
+      module NormalizePlugin
+        extend PluginApi
+
+        add_component(
+          name: :'normalize.css',
+          version: 'latest',
+          importable_files: %w(normalize.css),
+        )
+      end
+    end
+  end
+end
