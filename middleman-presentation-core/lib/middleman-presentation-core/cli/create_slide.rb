7346c94324d7d139d56b156fa63203d6616aefd3
Dennis Günnewig <dg1@vrnetze.de>
2014-10-27 11:43:44 +0100

    Refactored create slides to make use of new methods shared_instance from base_group

--- middleman-presentation-core/lib/middleman-presentation-core/cli/create_slide.rb
+++ middleman-presentation-core/lib/middleman-presentation-core/cli/create_slide.rb
@@ -13,60 +13,33 @@ module Middleman
         def create_slide
           enable_debug_mode
 
-          fail ArgumentError, Middleman::Presentation.t('errors.missing_argument', argument: 'name') if names.blank?
-
-          shared_instance = proc { ::Middleman::Application.server.inst }.call
-
-          # This only exists when the config.rb sets it!
-          if shared_instance.extensions.key? :presentation
-            presentation_inst = shared_instance.extensions[:presentation]
-
-            existing_slides = Middleman::Presentation::SlideList.new(
-              Dir.glob(File.join(shared_instance.source_dir, presentation_inst.options.slides_directory, '**', '*')),
-              slide_builder: Middleman::Presentation::ExistingSlide,
-              base_path: shared_instance.source_dir
-            ) do |l|
-              l.transform_with Middleman::Presentation::Transformers::FileKeeper.new
-            end
-
-            slide_list = Middleman::Presentation::SlideList.new(
-              names,
-              slide_builder: Middleman::Presentation::NewSlide,
-              base_path: File.join(shared_instance.source_dir, presentation_inst.options.slides_directory)
-            ) do |l|
-              l.transform_with Middleman::Presentation::Transformers::RemoveDuplicateSlides.new(additional_slides: existing_slides, raise_error: options[:error_on_duplicates])
-              l.transform_with Middleman::Presentation::Transformers::SortSlides.new
-            end
-
-            slide_list.each_existing do |slide|
-              $stderr.puts format('%-20s %-s', 'exist'.colorize(color: :blue, mode: :bold), slide.relative_path)
-            end
-
-            slide_list.each_new do |slide|
-              $stderr.puts format('%-20s %-s', 'create'.colorize(color: :green, mode: :bold), slide.relative_path)
-              slide.write(title: options[:title])
-            end
-
-            data = if shared_instance.data.respond_to? :metadata
-                     shared_instance.data.metadata.dup
-                   else
-                     OpenStruct.new
-                   end
-
-            if options[:edit]
-              editor = []
-              begin
-                editor << Erubis::Eruby.new(options[:editor_command]).result(data)
-              rescue NameError => e
-                $stderr.puts Middleman::Presentation.t('errors.missing_data_attribute', message: e.message)
-              end
-              editor.concat slide_list.existing_slides
-
-              system(editor.join(' '))
-            end
-          else
-            fail Thor::Error, Middleman::Presentation.t('errors.extension_not_activated')
+          existing_slides = Middleman::Presentation::SlideList.new(
+            Dir.glob(File.join(shared_instance.source_dir, presentation_inst.options.slides_directory, '**', '*')),
+            slide_builder: Middleman::Presentation::ExistingSlide,
+            base_path: shared_instance.source_dir
+          ) do |l|
+            l.transform_with Middleman::Presentation::Transformers::FileKeeper.new
           end
+
+          slide_list = Middleman::Presentation::SlideList.new(
+            names,
+            slide_builder: Middleman::Presentation::NewSlide,
+            base_path: File.join(shared_instance.source_dir, presentation_inst.options.slides_directory)
+          ) do |l|
+            l.transform_with Middleman::Presentation::Transformers::RemoveDuplicateSlides.new(additional_slides: existing_slides, raise_error: options[:error_on_duplicates])
+            l.transform_with Middleman::Presentation::Transformers::SortSlides.new
+          end
+
+          slide_list.each_existing do |slide|
+            $stderr.puts format('%-20s %-s', 'exist'.colorize(color: :blue, mode: :bold), slide.relative_path)
+          end
+
+          slide_list.each_new do |slide|
+            $stderr.puts format('%-20s %-s', 'create'.colorize(color: :green, mode: :bold), slide.relative_path)
+            slide.write(title: options[:title])
+          end
+
+          open_in_editor slide_list.existing_slides if options[:edit]
         end
       end
     end
