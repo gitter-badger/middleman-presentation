7cfb79ede24a272a5c2b856294fdfd93e9f7ade6
Dennis Günnewig <dg1@vrnetze.de>
2014-10-27 11:40:04 +0100

    Refactor change_slide

--- middleman-presentation-core/lib/middleman-presentation-core/cli/base_group.rb
+++ middleman-presentation-core/lib/middleman-presentation-core/cli/base_group.rb
@@ -12,6 +12,38 @@ module Middleman
           def enable_debug_mode
             Middleman::Presentation.enable_debug_mode if options[:debug_mode] == true
           end
+
+          def shared_instance
+            middleman_instance ||= proc { ::Middleman::Application.server.inst }.call
+
+            fail Thor::Error, Middleman::Presentation.t('errors.extension_not_activated') unless middleman_instance.extensions.key? :presentation
+
+            middleman_instance
+          end
+
+          def presentation_inst
+            shared_instance.extensions[:presentation]
+          end
+
+          def open_in_editor(paths)
+            data = if shared_instance.data.respond_to?(:metadata)
+                     shared_instance.data.metadata.dup
+                   else
+                     OpenStruct.new
+                   end
+
+            editor = []
+
+            begin
+              editor << Erubis::Eruby.new(options[:editor_command]).result(data)
+            rescue NameError => e
+              $stderr.puts Middleman::Presentation.t('errors.missing_data_attribute', message: e.message)
+            end
+            editor.concat paths
+
+            Middleman::Presentation.logger.warn Middleman::Presentation.t('infos.open_slide_in_editor', editor: editor.first)
+            system(editor.join(' '))
+          end
         end
       end
     end
--- middleman-presentation-core/lib/middleman-presentation-core/cli/change_slide.rb
+++ middleman-presentation-core/lib/middleman-presentation-core/cli/change_slide.rb
@@ -10,17 +10,11 @@ module Middleman
         class_option :base_name, desc: Middleman::Presentation.t('views.slides.change.options.base_name')
         class_option :type, desc: Middleman::Presentation.t('views.slides.change.options.type')
 
-        argument :names, type: :array, desc: Middleman::Presentation.t('views.slides.change.arguments.names')
+        argument :names, required: false, type: :array, desc: Middleman::Presentation.t('views.slides.change.arguments.names')
         def change_slide
           enable_debug_mode
 
           fail ArgumentError, Middleman::Presentation.t('errors.too_many_arguments', count: 1) if names.count > 1 && options.key?('base_name')
-          fail ArgumentError, Middleman::Presentation.t('errors.missing_argument', argument: 'name') if names.blank?
-
-          shared_instance = proc { ::Middleman::Application.server.inst }.call
-          fail Thor::Error, Middleman::Presentation.t('errors.extension_not_activated') unless shared_instance.extensions.key? :presentation
-
-          presentation_inst = shared_instance.extensions[:presentation]
 
           existing_slides = SlideList.new(
             Dir.glob(File.join(shared_instance.source_dir, presentation_inst.options.slides_directory, '**', '*')),
@@ -30,16 +24,20 @@ module Middleman
             l.transform_with Transformers::FileKeeper.new
           end
 
-          slides = []
-
-          names.each do |n|
-            old_slide = existing_slides.find { |s| s.base_name == n }
-
-            unless old_slide
-              Middleman::Presentation.logger.warn Middleman::Presentation.t('errors.slide_not_found', base_name: n)
-              next
+          found_slides = existing_slides.find_all do |s| 
+            if names.blank?
+              true
+            else
+              names.include?(s.base_name)
             end
+          end
 
+          if found_slides.blank? || found_slides.size != names.size
+            missing_slides = names - found_slides.map(&:base_name)
+            Middleman::Presentation.logger.warn Middleman::Presentation.t('errors.slide_not_found', base_name: missing_slides.to_list)
+          end
+
+          move_jobs = found_slides.map do |old_slide|
             new_slide_file_name = SlideName.new(
               old_slide,
               base_name: options[:base_name],
@@ -51,35 +49,20 @@ module Middleman
               base_path: File.join(shared_instance.source_dir, presentation_inst.options.slides_directory)
             )
 
-            slides << OpenStruct.new(old_slide: old_slide, new_slide: new_slide)
+            OpenStruct.new(
+              source: old_slide.path, 
+              source_short: old_slide.relative_path,
+              destination: new_slide.path,
+              destination_short: new_slide.relative_path
+            )
           end
 
-          slides.each do |s|
-            old_slide = s.old_slide
-            new_slide = s.new_slide
-
-            $stderr.puts format('%-20s %-s -> %-s', 'rename '.colorize(color: :blue, mode: :bold), old_slide.relative_path, new_slide.relative_path)
-            FileUtils.mv old_slide.path, new_slide.path
+          move_jobs.each do |j|
+            $stderr.puts format('%-20s %-s -> %-s', 'rename '.colorize(color: :blue, mode: :bold), j.source_short, j.destination_short)
+            FileUtils.mv j.source, j.destination
           end
 
-          return unless options[:edit]
-
-          data = if shared_instance.data.respond_to? :metadata
-                   shared_instance.data.metadata.dup
-                 else
-                   OpenStruct.new
-                 end
-
-          editor = []
-          begin
-            editor << Erubis::Eruby.new(options[:editor_command]).result(data)
-          rescue NameError => e
-            $stderr.puts Middleman::Presentation.t('errors.missing_data_attribute', message: e.message)
-          end
-          editor.concat slides.map { |s| s.new_slide.path }
-
-          Middleman::Presentation.logger.warn Middleman::Presentation.t('infos.open_slide_in_editor', editor: editor.first)
-          system(editor.join(' '))
+          open_in_editor move_jobs.map(&:destination).sort if options[:edit]
         end
       end
     end
