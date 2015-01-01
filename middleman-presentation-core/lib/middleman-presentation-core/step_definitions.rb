729056a610e84e531959ed320b076d263badf2e2
Max Meyer <dev@fedux.org>
2015-01-01 21:28:40 +0100

    Move code to helper method to make it reusable

--- middleman-presentation-core/lib/middleman-presentation-core/step_definitions.rb
+++ middleman-presentation-core/lib/middleman-presentation-core/step_definitions.rb
@@ -39,21 +39,7 @@ When(/^I start debugging/) do
 end
 
 Given(/^I use presentation fixture "([^"]+)" with title "([^"]+)"(?: and date "([^"]+)")?$/) do |name, title, date|
-  directory = []
-  directory << name
-  directory << ('-' + title)
-  directory << ('-' + date) if date
-
-  directory = directory.join.characterize
-
-  command = []
-  command << "middleman-presentation create presentation #{temporary_fixture_path(directory)}"
-  command << "--title #{Shellwords.escape(title)}"
-  command << "--date #{Shellwords.escape(date)}" if date
-
-  system(command.join(' ')) unless temporary_fixture_exist?(directory)
-
-  FileUtils.cp_r temporary_fixture_path(directory), absolute_path(name)
+  create_presentation(name, title, date)
   step %(I cd to "#{name}")
 end
 
--- middleman-presentation-core/lib/middleman-presentation-core/test_helpers.rb
+++ middleman-presentation-core/lib/middleman-presentation-core/test_helpers.rb
@@ -1,4 +1,6 @@
 # encoding: utf-8
+require 'fedux_org_stdlib/core_ext/string/characterize'
+
 module Middleman
   module Presentation
     # Test helpers
@@ -14,6 +16,24 @@ module Middleman
 
       def temporary_fixture_exist?(name)
         File.exist? File.expand_path("../../../tmp/fixtures/#{name}", __FILE__)
+      end
+
+      def create_presentation(name, title, date)
+        directory = []
+        directory << name
+        directory << ('-' + title)
+        directory << ('-' + date) if date
+
+        directory = directory.join.characterize
+
+        command = []
+        command << "middleman-presentation create presentation #{temporary_fixture_path(directory)}"
+        command << "--title #{Shellwords.escape(title)}"
+        command << "--date #{Shellwords.escape(date)}" if date
+
+        system(command.join(' ')) unless temporary_fixture_exist?(directory)
+
+        FileUtils.cp_r temporary_fixture_path(directory), absolute_path(name)
       end
 
       module_function :temporary_fixture_path
