c1b6bf22e5afb1a57d63f1fdb89559ed8b5fe815
Max Meyer <dev@fedux.org>
2015-01-02 09:57:26 +0100

    Moved code to helpers gem

--- middleman-presentation-core/features/support/env.rb
+++ middleman-presentation-core/features/support/env.rb
@@ -8,12 +8,10 @@ SimpleCov.start
 # middleman
 require 'middleman-core'
 require 'middleman-core/step_definitions'
-require 'middleman-core/step_definitions/builder_steps'
-require 'middleman-core/step_definitions/middleman_steps'
 # require 'middleman-presentation-core/middleman_step_definitions'
 
 # middleman-presentation
-require 'middleman-presentation-core/step_definitions'
+require 'middleman-presentation-helpers/step_definitions'
 
 # Pull in all of the gems including those in the `test` group
 require 'bundler'
